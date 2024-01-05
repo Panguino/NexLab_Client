9b66886977361f6d2f844a0bbfbee368d2a06fdd
Brad Larson <brad@bannerwave.com>
2024-01-05 10:26:43 -0700

    Update Navigation

--- src/components/layout/Navigation/Navigation.tsx
+++ src/components/layout/Navigation/Navigation.tsx
@@ -5,7 +5,10 @@ const Navigation = () => {
 	return (
 		<S.Navigation>
 			<Link href="/">
-				<S.Logo />
+				<S.Logo>
+					<img src="/img/logo-cloud-filled.svg" />
+					<img src="/img/logo-text.svg" />
+				</S.Logo>
 			</Link>
 			<S.NavItems>
 				<Link href="/data/">
@@ -22,6 +25,14 @@ const Navigation = () => {
 				</Link>
 				<Link href="/faqs/">
 					<S.NavItem>FAQs</S.NavItem>
+				</Link>
+			</S.NavItems>
+			<S.NavItems>
+				<Link href="/login/">
+					<S.NavItem>Login</S.NavItem>
+				</Link>
+				<Link href="/signup/">
+					<S.NavItem>Signup</S.NavItem>
 				</Link>
 			</S.NavItems>
 		</S.Navigation>
--- src/components/layout/SubNavigation/SubNavigation.styles.ts
+++ src/components/layout/SubNavigation/SubNavigation.styles.ts
@@ -1,18 +1,24 @@
 import styled from 'styled-components'
 
 export const SubNavigation = styled.div`
-    width: 100%;
-    padding: 10px;
-    display:flex;
-    justify-content: center;
-    background-color: #333;
-    color:#fff;     
-    a {
-        color: white;
-    }
+	width: 100%;
+	padding: 10px;
+	display: flex;
+	justify-content: center;
+	background-color: #333;
+	color: #fff;
+	a {
+		color: white;
+		text-decoration: none;
+		transition: color 0.2s ease;
+		&:hover {
+			color: #0ec5ff;
+			text-decoration: none;
+		}
+	}
 `
 export const NavItems = styled.div`
-    display:flex;
-    gap: 20px;
-    align-items:center;
+	display: flex;
+	gap: 20px;
+	align-items: center;
 `
