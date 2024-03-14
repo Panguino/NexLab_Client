e110dc3d85ab99c1dc6fac97a0a312574afe025c
Pete DeGraw <petedegraw@icloud.com>
2024-03-14 14:59:01 -0400

    Revert "Degree updates"
    This reverts commit 22a3d074502faafd72163de095a28ce3db83a537.

--- src/components/blocks/DegreeInfo/DegreeInfo.tsx
+++ src/components/blocks/DegreeInfo/DegreeInfo.tsx
@@ -1,18 +1,20 @@
 import { RichText } from '@/components/elements/RichText/RichText'
-import { Button } from '@/components/elements/Button/Button'
+
 import styles from './DegreeInfo.module.scss'
 
 export const DegreeInfo = ({ Title, Buttons, Description }) => {
 	return (
 		<div className={styles.DegreeInfo}>
 			<div className={styles.Info}>
-				<h2>{Title}</h2>
-				<div className={styles.ButtonGroup}>
-					{Buttons.map((button, index) => {
-						const { Label, Link, Style } = button
-						return <Button key={index} bURL={Link} Style={Style} Label={Label} />
-					})}
-				</div>
+				<h3>{Title}</h3>
+				{Buttons.map((button, index) => {
+					const { Label, Link, Style } = button
+					return (
+						<a key={index} href={Link} className={Style}>
+							{Label}
+						</a>
+					)
+				})}
 			</div>
 			<div className={styles.Description}>
 				<RichText text={Description} />
