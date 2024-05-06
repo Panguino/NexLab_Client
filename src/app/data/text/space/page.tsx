00e8edd812dc08950113fd883ab92c16efb5935a
Brad Larson <brad@bannerwave.com>
2024-05-06 09:40:18 -0400

    Sidebar POC

--- src/app/data/text/space/page.tsx
+++ src/app/data/text/space/page.tsx
@@ -1,7 +1,7 @@
 const Page = () => {
 	return (
 		<div>
-			<h1>Miscellaneous</h1>
+			<h1>Space</h1>
 		</div>
 	)
 }
---
+++ src/components/elements/SidebarSectionHeader/SidebarSectionHeader.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import styles from './SidebarSectionHeader.module.scss'
+
+export const SidebarSectionHeader = ({ name, linkUrl }) => {
+	return (
+		<div className={styles.SidebarSectionHeader}>
+			<Link href={linkUrl}>
+				{name}
+				<svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path
+						fillRule="evenodd"
+						clipRule="evenodd"
+						d="M0.963555 11.6863C0.612083 11.3349 0.612083 10.765 0.963555 10.4136L5.12716 6.24995L0.963555 2.08635C0.612083 1.73488 0.612083 1.16503 0.963555 0.813555C1.31503 0.462084 1.88487 0.462084 2.23635 0.813555L7.03635 5.61356C7.38782 5.96503 7.38782 6.53488 7.03635 6.88635L2.23635 11.6863C1.88488 12.0378 1.31503 12.0378 0.963555 11.6863Z"
+					/>
+				</svg>
+			</Link>
+		</div>
+	)
+}
