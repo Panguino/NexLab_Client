afd1ef421276f1aeab191092672fd67f974fcd95
Brad Larson <brad@bannerwave.com>
2024-05-16 18:06:50 -0400

    removed regional type of pages

--- src/app/weather-data/analysis/upper-air/continental-us/page.tsx
+++
@@ -1,9 +0,0 @@
-const Page = () => {
-	return (
-		<div>
-			<h1>Continental US</h1>
-		</div>
-	)
-}
-
-export default Page
--- src/app/weather-data/analysis/upper-air/soundings/alaska/page.tsx
+++
@@ -1,9 +0,0 @@
-const Page = () => {
-	return (
-		<div>
-			<h1>Alaska</h1>
-		</div>
-	)
-}
-
-export default Page
