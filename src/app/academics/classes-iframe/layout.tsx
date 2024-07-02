fc119732996adec67af3b0ae7dc9c6b2b3911e4f
Brad Larson <brad@bannerwave.com>
2024-07-02 15:35:08 -0400

    fix urls for iframe

--- src/app/academics/classes-iframe/layout.tsx
+++ src/app/academics/classes-iframe/layout.tsx
@@ -38,7 +38,13 @@ export default function Layout({ children }) {
 									<SidebarGroup key={index} title={Name}>
 										{coursesData.map((course, index) => {
 											const { CourseID } = course.attributes
-											return <SidebarLink key={index} linkUrl={`/academics/classes/${course.id}`} name={`Earth ${CourseID}`} />
+											return (
+												<SidebarLink
+													key={index}
+													linkUrl={`/academics/classes-iframe/${course.id}`}
+													name={`Earth ${CourseID}`}
+												/>
+											)
 										})}
 									</SidebarGroup>
 								)
