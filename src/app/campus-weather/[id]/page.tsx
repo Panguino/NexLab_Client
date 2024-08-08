bfc7c1ac2c975a863a130d20034b11823373a60d
Evan Anderson <elusiveuphoria@gmail.com>
2024-08-08 04:47:24 -0500

    Branch from staging
    something was wrong with "moment" on dev

--- src/app/campus-weather/[id]/page.tsx
+++ src/app/campus-weather/[id]/page.tsx
@@ -1,5 +1,40 @@
-const Page = ({ params }) => {
-	return <>Campus {params.id}</>
+import { gql } from '@apollo/client'
+
+import { getClient } from '@/apollo/apollo-client'
+import PageContentWrapper from '@/components/layout/PageContentWrapper/PageContentWrapper'
+
+const Page = async ({ params }) => {
+	const response = await getClient().query({
+		query: gql`
+			query {
+				campus(id: ${params.id}) {
+					data {
+						id
+						attributes {							
+							Name
+							Latitude
+							Longitude
+							Logo {
+								data {
+									attributes {
+										url
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		`
+	})
+	console.log({ response })
+	// return <>Campus {params.id}</>
+	const { Name, Latitude, Longitude } = response.data.campus.data.attributes
+	return (
+		<PageContentWrapper>
+			{Name}, {Latitude}, {Longitude}
+		</PageContentWrapper>
+	)
 }
 
 export default Page
