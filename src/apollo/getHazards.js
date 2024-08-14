98c026bacef38bf4f5a0caa94a2b3cdbb6e6d42f
Brad Larson <brad@bannerwave.com>
2024-08-14 14:44:14 -0400

    Fix build errors

--- src/apollo/getHazards.js
+++ src/apollo/getHazards.js
@@ -1,7 +1,6 @@
 'use server'
 import { gql } from '@apollo/client'
 import { getDataClient } from '@/apollo/apollo-client'
-import { rewind } from '@turf/turf'
 
 export const getHazards = async () => {
 	const conusCountiesData = await getDataClient().query({
@@ -106,48 +105,5 @@ export const getHazards = async () => {
 		`
 	})
 
-	const alerts = {}
-	conusCountiesData.data.getRegions.forEach((region) => {
-		alerts[region.name] = {}
-		region.states.forEach((state) => {
-			state.counties.forEach((county) => {
-				if (county.alerts.length != 0) {
-					const shapeFeature = { type: county.type, geometry: county.geometry, properties: {} }
-					alerts[region.name][county.properties.ID] = {
-						shape: rewind(shapeFeature, { reverse: true }),
-						alerts: county.alerts.map((alert) => {
-							return alert.properties
-						}),
-						properties: county.properties
-					}
-				}
-			})
-		})
-		region.coasts.forEach((coast) => {
-			if (coast.alerts.length != 0) {
-				const shapeFeature = { type: coast.type, geometry: coast.geometry, properties: {} }
-				alerts[region.name][coast.properties.ID] = {
-					shape: rewind(shapeFeature, { reverse: true }),
-					alerts: coast.alerts.map((alert) => {
-						return alert.properties
-					}),
-					properties: coast.properties
-				}
-			}
-		})
-		region.offshores.forEach((offshore) => {
-			if (offshore.alerts.length != 0) {
-				const shapeFeature = { type: offshore.type, geometry: offshore.geometry, properties: {} }
-				alerts[region.name][offshore.properties.ID] = {
-					shape: rewind(shapeFeature, { reverse: true }),
-					alerts: offshore.alerts.map((alert) => {
-						return alert.properties
-					}),
-					properties: offshore.properties
-				}
-			}
-		})
-	})
-
-	return alerts
+	return conusCountiesData
 }
--- src/components/blocks/Hazards/Hazards.tsx
+++ src/components/blocks/Hazards/Hazards.tsx
@@ -5,6 +5,7 @@ import HazardsTable from './HazardsTable/HazardsTable'
 import { getHazards } from '@/apollo/getHazards'
 import { useEffect } from 'react'
 import { useInterval } from '@/hooks/useInterval'
+import { rewind } from '@turf/turf'
 
 const Hazards = ({ displayRegions, displayStates, displayOffshores, alerts }) => {
 	const selectedView = useRootStore.use.selectedView()
@@ -22,7 +23,49 @@ const Hazards = ({ displayRegions, displayStates, displayOffshores, alerts }) =>
 
 	const refreshAlertData = async () => {
 		if (hazardRefreshActive && !slideoutPanelIsOpen) {
-			const alerts = await getHazards()
+			const conusCountiesData = await getHazards()
+			const alerts = {}
+			conusCountiesData.data.getRegions.forEach((region) => {
+				alerts[region.name] = {}
+				region.states.forEach((state) => {
+					state.counties.forEach((county) => {
+						if (county.alerts.length != 0) {
+							const shapeFeature = { type: county.type, geometry: county.geometry, properties: {} }
+							alerts[region.name][county.properties.ID] = {
+								shape: rewind(shapeFeature, { reverse: true }),
+								alerts: county.alerts.map((alert) => {
+									return alert.properties
+								}),
+								properties: county.properties
+							}
+						}
+					})
+				})
+				region.coasts.forEach((coast) => {
+					if (coast.alerts.length != 0) {
+						const shapeFeature = { type: coast.type, geometry: coast.geometry, properties: {} }
+						alerts[region.name][coast.properties.ID] = {
+							shape: rewind(shapeFeature, { reverse: true }),
+							alerts: coast.alerts.map((alert) => {
+								return alert.properties
+							}),
+							properties: coast.properties
+						}
+					}
+				})
+				region.offshores.forEach((offshore) => {
+					if (offshore.alerts.length != 0) {
+						const shapeFeature = { type: offshore.type, geometry: offshore.geometry, properties: {} }
+						alerts[region.name][offshore.properties.ID] = {
+							shape: rewind(shapeFeature, { reverse: true }),
+							alerts: offshore.alerts.map((alert) => {
+								return alert.properties
+							}),
+							properties: offshore.properties
+						}
+					}
+				})
+			})
 			setAllHazards(alerts)
 		}
 	}
