04669d6febf203f8f0c91a292e2330948c529f69
Brad Larson <brad@bannerwave.com>
2024-09-04 22:50:51 -0400

    static menu working

--- src/apollo/strapi/getMobileMenu.ts
+++ src/apollo/strapi/getMobileMenu.ts
@@ -11,7 +11,7 @@ export const getMobileMenu = async () => {
 						attributes {
 							title
 							slug
-							items {
+							items(pagination: { limit: 1000 }) {
 								data {
 									id
 									attributes {
--- src/components/layout/MobileMenu/MobileMenu.tsx
+++ src/components/layout/MobileMenu/MobileMenu.tsx
@@ -1,11 +1,13 @@
 'use client'
 import { useRootStore } from '@/store/useRootStore'
 import { circOut, motion } from 'framer-motion'
+import { useState } from 'react'
 import styles from './MobileMenu.module.scss'
 
 const MobileMenu = ({ children, navItems }) => {
 	const mobileMenuIsOpen = useRootStore.use.mobileMenuIsOpen()
 
+	const [menuId, setMenuId] = useState(null)
 	// const [activeMenu, setActiveMenu] = useState([{ items, name: 'main' }]) // Stack of menus
 
 	// const goDeeper = (subItems, name) => {
@@ -15,8 +17,15 @@ const MobileMenu = ({ children, navItems }) => {
 	// const goBack = () => {
 	// 	setActiveMenu(activeMenu.slice(0, activeMenu.length - 1))
 	// }
-	console.log('menuItems', navItems)
+	//console.log('menuItems', navItems)
 
+	const ifMenuItemHasChildren = (menuItemId) => {
+		return navItems.some((navItem) => navItem.attributes.parent.data?.id === menuItemId)
+	}
+	const getParentIdByChildMenuId = (childMenuId) => {
+		return navItems.find((navItem) => navItem.id === childMenuId).attributes.parent.data?.id || null
+	}
+	console.log('menuId', menuId)
 	return (
 		<motion.div
 			className={styles.MobileMenu}
@@ -25,11 +34,23 @@ const MobileMenu = ({ children, navItems }) => {
 			transition={{ duration: mobileMenuIsOpen ? 0.4 : 0.25, ease: circOut }}
 		>
 			{children}
-			{navItems.map((item) => (
-				<div key={item.id}>
-					{item.attributes.title} - {item.attributes.url}
-				</div>
-			))}
+			{menuId !== null && ifMenuItemHasChildren(menuId) && <div onClick={() => setMenuId(getParentIdByChildMenuId(menuId))}>-- Back</div>}
+			{navItems
+				.filter((item) => item.attributes.parent.data?.id === menuId || item.attributes.parent.data === menuId)
+				.map((item) => {
+					return (
+						<div
+							key={item.id}
+							onClick={() => {
+								if (ifMenuItemHasChildren(item.id)) {
+									setMenuId(item.id)
+								}
+							}}
+						>
+							{item.attributes.title} - {item.attributes.url} ---- {ifMenuItemHasChildren(item.id) && 'has children'}
+						</div>
+					)
+				})}
 		</motion.div>
 	)
 }
