8c9df678d89519941d15ec40c06063f51b7046a1
Evan Anderson <elusiveuphoria@gmail.com>
2024-09-05 05:31:09 -0500

    sidebar refactor
    in progress

--- src/apollo/strapi/getCampusLinks.ts
+++ src/apollo/strapi/getCampusLinks.ts
@@ -3,7 +3,7 @@ import { getClient } from '@/apollo/apollo-client'
 import { gql } from '@apollo/client'
 
 export const getCampusLinks = async () => {
-	const essentialsResponse = await getClient().query({
+	const campusLinksResponse = await getClient().query({
 		query: gql`
 			query {
 				menusMenu(id: "2") {
@@ -11,7 +11,7 @@ export const getCampusLinks = async () => {
 						attributes {
 							title
 							slug
-							items {
+							items(pagination: { limit: 1000 }) {
 								data {
 									id
 									attributes {
@@ -32,67 +32,5 @@ export const getCampusLinks = async () => {
 			}
 		`,
 	})
-	const helpfulResponse = await getClient().query({
-		query: gql`
-			query {
-				menusMenu(id: "3") {
-					data {
-						attributes {
-							title
-							slug
-							items {
-								data {
-									id
-									attributes {
-										title
-										url
-										target
-										parent {
-											data {
-												id
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		`,
-	})
-	const safetyResponse = await getClient().query({
-		query: gql`
-			query {
-				menusMenu(id: "4") {
-					data {
-						attributes {
-							title
-							slug
-							items {
-								data {
-									id
-									attributes {
-										title
-										url
-										target
-										parent {
-											data {
-												id
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		`,
-	})
-	return [
-		essentialsResponse.data.menusMenu.data.attributes.items.data,
-		helpfulResponse.data.menusMenu.data.attributes.items.data,
-		safetyResponse.data.menusMenu.data.attributes.items.data,
-	]
+	return campusLinksResponse.data.menusMenu.data.attributes.items.data
 }
--- src/app/campus-weather/layout.tsx
+++ src/app/campus-weather/layout.tsx
@@ -1,8 +1,6 @@
 'use client'
 
 import { getCampusLinks } from '@/apollo/strapi/getCampusLinks'
-import { LinkAccordian } from '@/components/elements/LinkAccordian/LinkAccordian'
-import { LinkAccordianItem } from '@/components/elements/LinkAccordian/LinkAccordianItem/LinkAccordianItem'
 import ScrollArea from '@/components/layout/ScrollArea/ScrollArea'
 import SidebarNavigation from '@/components/layout/SidebarNavigation/SidebarNavigation'
 import SidebarWrapper from '@/components/layout/SidebarWrapper/SidebarWrapper'
@@ -10,9 +8,6 @@ import { useEffect, useState } from 'react'
 
 export default function Layout({ children }) {
 	const [campusLinks, setCampusLinks] = useState([])
-	const essentialLinks = campusLinks[0]
-	const helpfulLinks = campusLinks[1]
-	const safetyLinks = campusLinks[2]
 	useEffect(() => {
 		const fetchData = async () => {
 			const res = await getCampusLinks()
@@ -20,31 +15,12 @@ export default function Layout({ children }) {
 		}
 		fetchData().catch(console.error)
 	}, [])
-	console.log(essentialLinks, helpfulLinks, safetyLinks)
+	console.log(campusLinks)
 	return (
 		<SidebarWrapper>
 			<SidebarNavigation>
 				<ScrollArea>
-					<div style={{ padding: '10px 20px 30px 20px' }}>
-						<LinkAccordian title="Important Essentials">
-							{essentialLinks.map((campusLink, index) => {
-								const { title, url } = campusLink.attributes
-								return <LinkAccordianItem key={index} link={url} name={title} />
-							})}
-						</LinkAccordian>
-						<LinkAccordian title="Helpful Information">
-							{helpfulLinks.map((campusLink, index) => {
-								const { title, url } = campusLink.attributes
-								return <LinkAccordianItem key={index} link={url} name={title} />
-							})}
-						</LinkAccordian>
-						<LinkAccordian title="Weather Safety">
-							{safetyLinks.map((campusLink, index) => {
-								const { title, url } = campusLink.attributes
-								return <LinkAccordianItem key={index} link={url} name={title} />
-							})}
-						</LinkAccordian>
-					</div>
+					<div style={{ padding: '10px 20px 30px 20px' }}>sidebar content</div>
 				</ScrollArea>
 			</SidebarNavigation>
 			{children}
