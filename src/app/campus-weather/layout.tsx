c945acef0da793316d67b2c340a87408cc88d156
Evan Anderson <elusiveuphoria@gmail.com>
2024-09-05 05:45:46 -0500

    Campus sidebar refactor progress
    proper query and ingest into layout, almost outputing correctly

--- src/app/campus-weather/layout.tsx
+++ src/app/campus-weather/layout.tsx
@@ -16,11 +16,39 @@ export default function Layout({ children }) {
 		fetchData().catch(console.error)
 	}, [])
 	console.log(campusLinks)
+
+	const [menuId, setMenuId] = useState(null)
+
+	const ifMenuItemHasChildren = (menuItemId) => {
+		return campusLinks.some((campusLink) => campusLink.attributes.parent.data?.id === menuItemId)
+	}
+	// const getParentIdByChildMenuId = (childMenuId) => {
+	// 	return campusLinks.find((campusLink) => campusLink.id === childMenuId).attributes.parent.data?.id || null
+	// }
 	return (
 		<SidebarWrapper>
 			<SidebarNavigation>
 				<ScrollArea>
-					<div style={{ padding: '10px 20px 30px 20px' }}>sidebar content</div>
+					<div style={{ padding: '10px 20px 30px 20px' }}>
+						{campusLinks
+							.filter((item) => item.attributes.parent.data?.id === menuId || item.attributes.parent.data === menuId)
+							.map((item) => {
+								return (
+									<div
+										key={item.id}
+										onClick={() => {
+											// I know i dont need this but it works
+											// need to output links right away and start using SidebarGroup/Link
+											if (ifMenuItemHasChildren(item.id)) {
+												setMenuId(item.id)
+											}
+										}}
+									>
+										{item.attributes.title} - {item.attributes.url} ---- {ifMenuItemHasChildren(item.id) && 'has children'}
+									</div>
+								)
+							})}
+					</div>
 				</ScrollArea>
 			</SidebarNavigation>
 			{children}
