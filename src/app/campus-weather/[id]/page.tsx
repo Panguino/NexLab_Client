d7b4422ce83556ccc8d1227cb9a682978005a670
Evan Anderson <112748748+elusiveuphoria@users.noreply.github.com>
2024-10-21 19:07:57 -0500

    Feature/nxl 7500198697 cod wxbug data (#34)
    * Nav + Subnav Responsive
    
    * Added breakpoints
    
    * Mobile Menu opens and closes
    
    * tweaked sizing and easing
    
    * Attempt CWX strapi/sidebar query
    
    Need assistence
    
    * navigation improvements
    
    * Iteration on sidebar
    
    May restructure menu structure in strapi
    
    * changed strapi to menus vs navigation
    
    * fix build error
    
    * add clientside apollo provider
    
    * menu iteration
    
    has errors
    
    * route updates
    
    * remove unused file
    
    * update navigation links
    
    * nexrad dual-pol
    
    * renamed satellite mosaic pages
    
    * static menu working
    
    * sidebar refactor
    
    in progress
    
    * Campus sidebar refactor progress
    
    proper query and ingest into layout, almost outputing correctly
    
    * Sidebar Working
    
    * Design change to SidebarGroup
    
    * Dots on demand
    
    SidebarGroup now with variable styling using 'styleType'
    
    * Using var colors
    
    * Initial feature commit
    
    successfully retrieving data
    
    * Fixed apparent temp bug
    
    * Working
    
    but dirty
    
    * Forking from Tiles
    
    * API Icon Conversion
    
    successfully converting API icons to new svg icons
    
    * Full Icon Conversion
    
    successfully converting both COD and API icons to new svg icons
    
    ---------
    
    Co-authored-by: Brad Larson <brad@bannerwave.com>

--- src/app/campus-weather/[id]/page.tsx
+++ src/app/campus-weather/[id]/page.tsx
@@ -2,6 +2,7 @@ import { getClient } from '@/apollo/apollo-client'
 import { NextPageProps } from '@/app/types'
 import { CampusWeatherDetail } from '@/components/blocks/CampusWeatherDetail/CampusWeatherDetail'
 import PageContentWrapper from '@/components/layout/PageContentWrapper/PageContentWrapper'
+import { celsiusToFahrenheit, getCompassDirection, kphToMph } from '@/util/unitConversion'
 import { gql } from '@apollo/client'
 
 const Page = async ({ params }: NextPageProps) => {
@@ -22,6 +23,7 @@ const Page = async ({ params }: NextPageProps) => {
                                     }
                                 }
                             }
+							uniqueWeatherConditions
                         }
                     }
                 }
@@ -49,43 +51,149 @@ const Page = async ({ params }: NextPageProps) => {
 
 	const api_point_data = await api_point_res.json()
 
-	// Our Office products
-	// adding that leading K is only a problem if somehow we expand this service outside the CONUS
-	// const cod_cwa = 'https://weather.cod.edu/textserv/office/K' + api_point_data.cwa
+	// Determine where current conditions are coming from
+	let current_conditions = null
+	if (response.data.campus.data.attributes.uniqueWeatherConditions) {
+		// Collect the COD WxBug Data
+		const cod_wxbug_call = 'https://climate.cod.edu/data/cwx/current/KCDP-current-conditions.json'
 
-	// Calling all stations...
-	const api_station_call = `${api_point_data.observationStations}`
+		// Fetch the weather data
+		const cod_wxbug_res = await fetch(cod_wxbug_call, {
+			headers: {
+				'User-Agent': 'College of DuPage - Meteorology: Campus Weather (wxstaff@weather.cod.edu)',
+				Accept: 'application/ld+json',
+			},
+		})
 
-	// Fetch the weather data
-	const api_station_res = await fetch(api_station_call, {
-		headers: {
-			'User-Agent': 'College of DuPage - Meteorology: Campus Weather (wxstaff@weather.cod.edu)',
-			Accept: 'application/ld+json',
-		},
-	})
+		if (!cod_wxbug_res.ok) {
+			throw new Error(`HTTP error! status: ${cod_wxbug_res.status}`)
+		}
 
-	if (!api_station_res.ok) {
-		throw new Error(`HTTP error! status: ${api_station_res.status}`)
+		const cod_wxbug_data = await cod_wxbug_res.json()
+
+		// console.log(cod_wxbug_data)
+		const dataSource = 'cod'
+		const temperature = cod_wxbug_data.temp
+		const dewpoint = cod_wxbug_data.dewp
+		const apparentTemperature = cod_wxbug_data.atemp
+		const relativeHumidity = cod_wxbug_data.rhum
+		const windSpeed = cod_wxbug_data.wind.mag
+		const windDirection = cod_wxbug_data.wind.dir.abbr
+		const textDescription = cod_wxbug_data.wx.wxtitle
+		const icon = cod_wxbug_data.wx.symbol
+		const dayNight = cod_wxbug_data.dayNight
+		const sky = cod_wxbug_data.sky < 8 ? 'bkn' : 'ovc'
+
+		current_conditions = {
+			dataSource,
+			dayNight,
+			temperature,
+			dewpoint,
+			apparentTemperature,
+			relativeHumidity,
+			windSpeed,
+			windDirection,
+			sky,
+			textDescription,
+			icon,
+		}
+
+		console.log({ cod_wxbug_data })
+	} else {
+		// Our Office products
+		// adding that leading K is only a problem if somehow we expand this service outside the CONUS
+		// const cod_cwa = 'https://weather.cod.edu/textserv/office/K' + api_point_data.cwa
+
+		// Calling all stations...
+		const api_station_call = `${api_point_data.observationStations}`
+
+		// Fetch the weather data
+		const api_station_res = await fetch(api_station_call, {
+			headers: {
+				'User-Agent': 'College of DuPage - Meteorology: Campus Weather (wxstaff@weather.cod.edu)',
+				Accept: 'application/ld+json',
+			},
+		})
+
+		if (!api_station_res.ok) {
+			throw new Error(`HTTP error! status: ${api_station_res.status}`)
+		}
+
+		const api_station_data = await api_station_res.json()
+
+		// Get Current Conditions from Nearest Observation Station, [0] = Nearest, limit=1 = Newest
+		const api_obs_call = `${api_station_data.observationStations[0]}/observations?limit=1`
+
+		// Fetch Current Conditions
+		const api_obs_res = await fetch(api_obs_call, {
+			headers: {
+				'User-Agent': 'College of DuPage - Meteorology: Campus Weather (wxstaff@weather.cod.edu)',
+				Accept: 'application/ld+json',
+			},
+		})
+
+		if (!api_obs_res.ok) {
+			throw new Error(`HTTP error! status: ${api_obs_res.status}`)
+		}
+
+		const api_obs_data = await api_obs_res.json()
+
+		// console.log(api_obs_data['@graph'][0])
+
+		let { temperature } = api_obs_data['@graph'][0]
+		const { windChill, heatIndex } = api_obs_data['@graph'][0]
+
+		let apparentTemperature
+		if (windChill.value !== null) {
+			apparentTemperature = windChill.value
+		} else if (heatIndex.value !== null) {
+			apparentTemperature = heatIndex.value
+		} else {
+			apparentTemperature = temperature.value
+		}
+
+		const dataSource = 'api'
+		temperature = celsiusToFahrenheit(api_obs_data['@graph'][0].temperature.value)
+		const dewpoint = celsiusToFahrenheit(api_obs_data['@graph'][0].dewpoint.value)
+		apparentTemperature = celsiusToFahrenheit(apparentTemperature)
+		const relativeHumidity = api_obs_data['@graph'][0].relativeHumidity.value.toFixed(0)
+		const windSpeed = kphToMph(api_obs_data['@graph'][0].windSpeed.value)
+		const windDirection = getCompassDirection(api_obs_data['@graph'][0].windDirection.value)
+		const textDescription = api_obs_data['@graph'][0].textDescription
+		const icon = api_obs_data['@graph'][0].icon
+		// const dayNight = icon.includes('day') ? 'day' : 'night'
+		const dayNight = null // this isn't needed for api calls, just need var set to null
+		let sky = api_obs_data['@graph'][0].cloudLayers.amount
+
+		switch (sky) {
+			case 'CLR':
+			case 'FEW':
+			case 'SCT':
+			case 'BKN':
+				sky = 'bkn' // all treated as 'non-overcast'
+				break
+			case 'OVC':
+			case 'VV':
+				sky = 'ovc' // all treated as overcast
+				break
+			default:
+				sky = 'ovc' // totally fine to catch any anomalies this way
+				break
+		}
+		current_conditions = {
+			dataSource,
+			dayNight,
+			temperature,
+			dewpoint,
+			apparentTemperature,
+			relativeHumidity,
+			windSpeed,
+			windDirection,
+			sky,
+			textDescription,
+			icon,
+		}
 	}
-
-	const api_station_data = await api_station_res.json()
-
-	// Get Current Conditions from Nearest Observation Station, [0] = Nearest, limit=1 = Newest
-	const api_obs_call = `${api_station_data.observationStations[0]}/observations?limit=1`
-
-	// Fetch Current Conditions
-	const api_obs_res = await fetch(api_obs_call, {
-		headers: {
-			'User-Agent': 'College of DuPage - Meteorology: Campus Weather (wxstaff@weather.cod.edu)',
-			Accept: 'application/ld+json',
-		},
-	})
-
-	if (!api_obs_res.ok) {
-		throw new Error(`HTTP error! status: ${api_obs_res.status}`)
-	}
-
-	const api_obs_data = await api_obs_res.json()
 
 	// Get 7 day forecast data
 	const api_fcst_call = `${api_point_data.forecast}`
@@ -104,11 +212,13 @@ const Page = async ({ params }: NextPageProps) => {
 
 	const api_fcst_data = await api_fcst_res.json()
 
+	// console.log(api_fcst_data)
+
 	return (
 		<PageContentWrapper>
 			<CampusWeatherDetail
 				campusDetails={{ ...response.data.campus.data.attributes }}
-				currentWeatherData={api_obs_data['@graph'][0]}
+				currentWeatherData={current_conditions}
 				forecastData={api_fcst_data.periods}
 			/>
 		</PageContentWrapper>
--- src/components/blocks/CampusWeatherDetail/CurrentConditions/CurrentConditions.tsx
+++ src/components/blocks/CampusWeatherDetail/CurrentConditions/CurrentConditions.tsx
@@ -1,27 +1,32 @@
-import { celsiusToFahrenheit, getCompassDirection, kphToMph } from '@/util/unitConversion'
+import { convertIconName } from '@/util/getCampusWeatherIcon'
 import styles from './CurrentConditions.module.scss'
 import { CurrentValue } from './CurrentValue/CurrentValue'
 
 export const CurrentConditions = ({ currentWeatherData }) => {
-	const { temperature, dewpoint, windDirection, windSpeed, icon, relativeHumidity, windChill, heatIndex, textDescription } = currentWeatherData
-	let apparentTemperature
-	if (windChill.value !== null) {
-		apparentTemperature = windChill.value
-	} else if (heatIndex !== null) {
-		apparentTemperature = heatIndex.value
-	} else {
-		apparentTemperature = temperature.value
-	}
-
+	const {
+		dataSource,
+		dayNight,
+		temperature,
+		dewpoint,
+		apparentTemperature,
+		relativeHumidity,
+		windSpeed,
+		windDirection,
+		sky,
+		textDescription,
+		icon,
+	} = currentWeatherData
+	const weatherIcon = icon !== null ? convertIconName(icon, sky, dayNight, dataSource) : ('/temp-icons/unknown.svg' as string)
+	// console.log(currentWeatherData)
 	return (
 		<div className={styles.CurrentConditionsContainer}>
-			<img src={icon} className={styles.weatherSymbol} />
-			<div className={styles.airTemperature}>{celsiusToFahrenheit(temperature.value)}&deg;F</div>
+			<img src={weatherIcon} className={styles.weatherSymbol} />
+			<div className={styles.airTemperature}>{temperature}&deg;F</div>
 			<div className={styles.weatherDescription}>{textDescription}</div>
-			<CurrentValue label="Feels Like" value={celsiusToFahrenheit(apparentTemperature) + '\u00B0F'} />
-			<CurrentValue label="Dewpoint" value={celsiusToFahrenheit(dewpoint.value) + '\u00B0F'} />
-			<CurrentValue label="Humidity" value={relativeHumidity.value.toFixed(0) + '%'} />
-			<CurrentValue label="Wind" value={kphToMph(windSpeed.value) + 'mph (' + getCompassDirection(windDirection.value) + ')'} />
+			<CurrentValue label="Feels Like" value={apparentTemperature + '\u00B0F'} />
+			<CurrentValue label="Dewpoint" value={dewpoint + '\u00B0F'} />
+			<CurrentValue label="Humidity" value={relativeHumidity + '%'} />
+			<CurrentValue label="Wind" value={windSpeed + 'mph (' + windDirection + ')'} />
 		</div>
 	)
 }
