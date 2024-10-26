94e8199cae6e526de902314f80e2b1353739ef58
Evan Anderson <elusiveuphoria@gmail.com>
2024-10-26 03:40:19 -0500

    Restoring file
    Possibly chose the wrong version from the conflict in the merge

--- src/components/blocks/CampusWeatherDetail/CampusWeatherDetail.tsx
+++ src/components/blocks/CampusWeatherDetail/CampusWeatherDetail.tsx
@@ -4,42 +4,6 @@ import { ForecastTile } from './ForecastTiles/ForecastTile'
 
 export const CampusWeatherDetail = ({ currentWeatherData, tileData, forecastData, campusDetails }) => {
 	const { Name, Logo } = campusDetails
-	const tileData = []
-
-	for (const [index, period] of forecastData.entries()) {
-		const tileObj = {}
-		const startTime = new Date(period.startTime)
-		const dateName = `${startTime.getMonth() + 1}/${startTime.getDate()}`
-
-		if (period.isDaytime) {
-			// naturally skips night periods in the loop, barring a leading night period
-			tileObj['title'] = `${period.name} ${dateName}`
-			tileObj['day'] = {}
-			tileObj['day']['icon'] = convertIconName(period.icon, null, 'day', 'api')
-			tileObj['day']['temperature'] = `${period.temperature}\u00B0`
-			tileObj['day']['wind'] = period.windSpeed
-			tileObj['day']['precip'] = period.probabilityOfPrecipitation.value
-			if (forecastData[index + 1] !== undefined) {
-				// since we skip night periods, we need to populate the night period now
-				tileObj['night'] = {}
-				tileObj['night']['icon'] = convertIconName(forecastData[index + 1].icon, null, 'day', 'api')
-				tileObj['night']['temperature'] = `${forecastData[index + 1].temperature}\u00B0`
-				tileObj['night']['wind'] = forecastData[index + 1].windSpeed
-				tileObj['night']['precip'] = forecastData[index + 1].probabilityOfPrecipitation.value
-			}
-			tileData.push(tileObj)
-		} else if (!period.isDaytime && forecastData[index - 1] === undefined) {
-			// catch a leading solo night period
-			// exclude date in title on this occasion, once passed midnight it will match "tomorrow's" date and appear confusing
-			tileObj['title'] = period.name.replace(' Night', '')
-			tileObj['night'] = {}
-			tileObj['night']['icon'] = convertIconName(period.icon, null, 'day', 'api')
-			tileObj['night']['temperature'] = `${period.temperature}\u00B0`
-			tileObj['night']['wind'] = period.windSpeed
-			tileObj['night']['precip'] = period.probabilityOfPrecipitation.value
-			tileData.push(tileObj)
-		}
-	}
 
 	return (
 		<div className={styles.CampusWeatherDetail}>
