6422f85210d4c666e0fa47b1529ac4d4084276db
Evan Anderson <112748748+elusiveuphoria@users.noreply.github.com>
2024-10-26 00:15:47 -0500

    Feature/nxl 7593148835 forecast tiles (#35)
    * Nav + Subnav Responsive
    
    * Added breakpoints
    
    * Mobile Menu opens and closes
    
    * tweaked sizing and easing
    
    * Attempt CWX strapi/sidebar query
    
    Need assistence
    
    * navigation improvements
    
    * Iteration on sidebar
    
    May restructure menu structure in strapi
    
    * changed strapi to menus vs navigation
    
    * fix build error
    
    * add clientside apollo provider
    
    * menu iteration
    
    has errors
    
    * route updates
    
    * remove unused file
    
    * update navigation links
    
    * nexrad dual-pol
    
    * renamed satellite mosaic pages
    
    * static menu working
    
    * sidebar refactor
    
    in progress
    
    * Campus sidebar refactor progress
    
    proper query and ingest into layout, almost outputing correctly
    
    * Sidebar Working
    
    * Design change to SidebarGroup
    
    * Dots on demand
    
    SidebarGroup now with variable styling using 'styleType'
    
    * Using var colors
    
    * Initial feature commit
    
    successfully retrieving data
    
    * Fixed apparent temp bug
    
    * Working
    
    but dirty
    
    * Initial Commit
    
    Changing Forecast to match Design
    
    * Forking from Tiles
    
    * API Icon Conversion
    
    successfully converting API icons to new svg icons
    
    * Full Icon Conversion
    
    successfully converting both COD and API icons to new svg icons
    
    * Prepping
    
    Getting ready to convert API forecast to tiles
    
    * API Forecast Tiles
    
    Tiled graphical forecast from NWS API with styling
    
    * Flex, Styling, Layout issues improved
    
    iteration toward more component building
    
    * Fcst Component and Landing
    
    Forecast Tile component with size variant, landing page started, footer added
    
    * Tiles Done
    
    Should have stopped for PR and branch...
    
    * fixed package issues
    
    * Improvements / cleanup
    
    ---------
    
    Co-authored-by: Brad Larson <brad@bannerwave.com>

--- src/app/campus-weather/page.tsx
+++ src/app/campus-weather/page.tsx
@@ -1,5 +1,75 @@
-const Page = () => {
-	return <>Campus Weather</>
+import { getClient } from '@/apollo/apollo-client'
+import { Button } from '@/components/elements/Button/Button'
+import { getAPIdataFromLocation, getAPIforecast, getAPIweatherConditions, getCODweatherConditions } from '@/util/getCampusWeatherData'
+import { gql } from '@apollo/client'
+
+const Page = async () => {
+	const response = await getClient().query({
+		query: gql`
+			query {
+				campuses {
+					data {
+						id
+						attributes {
+							Name
+							Latitude
+							Longitude
+							Logo {
+								data {
+									id
+									attributes {
+										url
+									}
+								}
+							}
+							uniqueWeatherConditions
+						}
+					}
+				}
+			}
+		`,
+	})
+	// console.log(response.data.campuses.data)
+	const campuses = response.data.campuses.data
+	let campusWeather = []
+
+	const fetchSources = async () => {
+		const source_promises = campuses.map(async (campus) => {
+			const campusAPIdata = await getAPIdataFromLocation(campus.attributes.Latitude, campus.attributes.Longitude)
+			let current_conditions = null
+			if (campus.attributes.uniqueWeatherConditions === true) {
+				// cod
+				current_conditions = await getCODweatherConditions()
+			} else {
+				// other
+				current_conditions = await getAPIweatherConditions(campusAPIdata)
+			}
+			const forecastData = await getAPIforecast(campusAPIdata)
+			return { id: campus.id, conditions: current_conditions, forecast: forecastData }
+		})
+
+		campusWeather = await Promise.all(source_promises)
+		return campusWeather
+	}
+
+	campusWeather = await fetchSources()
+	console.log(campusWeather)
+
+	return (
+		<div>
+			{campuses.map((campus) => (
+				<div key={campus.id}>
+					<h2>
+						<img src={campus.attributes.Logo.data.attributes.url} alt={campus.attributes.Name} />
+						{campus.attributes.Name}
+					</h2>
+					<p>Lat: {campus.attributes.Latitude}</p>
+					<p>Lon: {campus.attributes.Longitude}</p>
+					<Button label="View Weather" link={`/campus-weather/${campus.id}`} target="_self" />
+				</div>
+			))}
+		</div>
+	)
 }
 
 export default Page
--- src/components/blocks/CampusWeatherDetail/CampusWeatherDetail.tsx
+++ src/components/blocks/CampusWeatherDetail/CampusWeatherDetail.tsx
@@ -1,22 +1,34 @@
-// 'use client'
-
 import styles from './CampusWeatherDetail.module.scss'
 import { CurrentConditions } from './CurrentConditions/CurrentConditions'
+import { ForecastTile } from './ForecastTiles/ForecastTile'
 
-export const CampusWeatherDetail = ({ currentWeatherData, forecastData, campusDetails }) => {
+export const CampusWeatherDetail = ({ currentWeatherData, tileData, forecastData, campusDetails }) => {
 	const { Name, Logo } = campusDetails
 
 	return (
 		<div className={styles.CampusWeatherDetail}>
-			<h2 className={styles.CampusTitle}>{Name}</h2>
-			<CurrentConditions currentWeatherData={currentWeatherData} />
-			<div className={styles.Spacer}></div>
-			<img src={Logo.data.attributes.url} className={styles.CampusLogo} />
+			<div className={styles.topSectionContainer}>
+				<h2 className={styles.CampusTitle}>
+					<img src={Logo.data.attributes.url} className={styles.logo} />
+					{Name}
+				</h2>
+				<div className={styles.CurrentDataContainer}>
+					<div className={styles.conditionsContainer}>
+						<CurrentConditions currentWeatherData={currentWeatherData} />
+					</div>
+					<div className={styles.skycamContainer}>{/* <SkyCam /> */}</div>
+					<div className={styles.radarContainer}>{/* <Radar /> */}</div>
+				</div>
+			</div>
+			<div className={styles.forecastTiles}>
+				{tileData.map(({ title, dayData, nightData, size }, index) => (
+					<ForecastTile key={index} title={title} dayData={dayData} nightData={nightData} size={size} />
+				))}
+			</div>
 			<div className={styles.CampusForecastContainer}>
-				<h2>Forecast</h2>
 				{forecastData.map((period, index) => (
 					<div key={index} className={styles.period}>
-						<h3>{period.name}</h3>
+						<h2>{period.name}</h2>
 						<p>{period.detailedForecast}</p>
 					</div>
 				))}
