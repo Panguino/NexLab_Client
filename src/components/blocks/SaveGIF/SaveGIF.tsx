20bbc9b9190a9dc99b1e9a9a4eff0ad28c92fab3
Evan Anderson <elusiveuphoria@gmail.com>
2024-12-03 14:26:21 -0600

    Minor Improvments
    styling and partial refactor

--- src/components/blocks/SaveGIF/SaveGIF.tsx
+++ src/components/blocks/SaveGIF/SaveGIF.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import './SaveGIF.scss'
+import React, { useEffect, useState } from 'react'
+import styles from './SaveGIF.module.scss'
 
 interface SaveGIFProps {
 	gifSaveName?: string
@@ -10,6 +10,11 @@ interface SaveGIFProps {
 const SaveGIF: React.FC<SaveGIFProps> = ({ gifSaveName = 'animation.gif', onGifSave, framesAvailable }) => {
 	const [filename, setFilename] = useState(gifSaveName)
 	const [error, setError] = useState('')
+	const [isButtonDisabled, setIsButtonDisabled] = useState(!gifSaveName)
+
+	useEffect(() => {
+		setIsButtonDisabled(!filename)
+	}, [filename])
 
 	const handleSave = () => {
 		if (!framesAvailable) {
@@ -25,14 +30,17 @@ const SaveGIF: React.FC<SaveGIFProps> = ({ gifSaveName = 'animation.gif', onGifS
 	}
 
 	return (
-		<div className="save-gif-component">
-			<div className="input-wrapper">
-				<p className="instruction-text">
-					The GIF will use the current animation settings (e.g., frame rate, start/end frames, dwell times). Please adjust the settings in
-					the animator to achieve the desired output before saving.
-				</p>
-				<div className="filename-wrapper">
-					<label className="filename-label" htmlFor="filename-input">
+		<div className={styles.saveGifComponent}>
+			<div className={styles.inputWrapper}>
+				<div className={styles.instructionWrapper}>
+					<h4 className={styles.instructionTitle}>Instructions:</h4>
+					<p className={styles.instructionText}>
+						The GIF will use the current animation settings (e.g., frame rate, start/end frames, dwell times). Please adjust the settings
+						in the animator to achieve the desired output before saving.
+					</p>
+				</div>
+				<div className={styles.filenameWrapper}>
+					<label className={styles.filenameLabel} htmlFor="filename-input">
 						Filename:
 					</label>
 					<input
@@ -40,16 +48,16 @@ const SaveGIF: React.FC<SaveGIFProps> = ({ gifSaveName = 'animation.gif', onGifS
 						type="text"
 						value={filename}
 						onChange={(e) => setFilename(e.target.value)}
-						className="filename-input"
+						className={styles.filenameInput}
 					/>
 				</div>
-				<div className="save-button-wrapper">
-					<button onClick={handleSave} className="save-button">
+				<div className={styles.saveButtonWrapper}>
+					<button onClick={handleSave} className={styles.saveButton} disabled={isButtonDisabled}>
 						Save GIF
 					</button>
 				</div>
 			</div>
-			{error && <p className="error-message">{error}</p>}
+			{error && <p className={styles.errorMessage}>{error}</p>}
 		</div>
 	)
 }
