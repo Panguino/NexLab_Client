ef18e82ee98df0d7c4c3efd41f8d57297308ada4
Brad Larson <brad@bannerwave.com>
2024-12-13 11:55:27 -0500

    Select Storybook Story Improvements

--- src/components/elements/SectorSelector/SectorSelector.tsx
+++ src/components/elements/SectorSelector/SectorSelector.tsx
@@ -1,9 +1,9 @@
+import lakesJson from '@/data/d3Map/lakes.json'
+import statesJson from '@/data/d3Map/states.json'
+import mapJson from '@/data/d3Map/world.json'
 import * as d3 from 'd3'
 import React, { useEffect, useRef } from 'react'
 import styles from './SectorSelector.module.scss'
-import lakesJson from './lakes.json'
-import statesJson from './states.json'
-import mapJson from './world.json'
 
 export type d3ConfigProps = {
 	width: number
@@ -27,18 +27,21 @@ export type ISectorSelectorProps = {
 const SectorSelector: React.FC<ISectorSelectorProps> = ({ sectors, sector, onChange, d3config }) => {
 	const svgRef = useRef<SVGSVGElement | null>(null)
 
+	console.log('d3config', d3config)
+	console.log('sectors', sectors)
+	console.log('sector', sector)
+
 	useEffect(() => {
+		if (!d3config) return
+		const { width, height, scale, rotate } = d3config
 		const svg = d3.select(svgRef.current)
-		const width = d3config?.width || 1000
-		const height = d3config?.height || 600
 		const translate = [width / 2, height / 2]
-		// eslint-disable-next-line prettier/prettier
 		const projection = d3
 			.geoOrthographic() // like a 2d globe
 			.precision(0)
-			.scale(height * d3config.scale) // zoom
+			.scale(height * scale) // zoom
 			.translate(translate)
-			.rotate(d3config.rotate) // center projection by using inverse lat,lon
+			.rotate(rotate) // center projection by using inverse lat,lon
 		const path = d3.geoPath().projection(projection)
 
 		svg.selectAll('*').remove()
@@ -46,11 +49,9 @@ const SectorSelector: React.FC<ISectorSelectorProps> = ({ sectors, sector, onCha
 		svg.append('g').selectAll('path').data(mapJson.features).enter().append('path').attr('d', path).attr('class', styles.mapPath)
 
 		const lakesGroup = svg.append('g')
-		// eslint-disable-next-line prettier/prettier
 		lakesGroup.selectAll('path.lakePath').data(lakesJson.features).enter().append('path').attr('d', path).attr('class', styles.lakePath)
 
 		const statesGroup = svg.append('g')
-		// eslint-disable-next-line prettier/prettier
 		statesGroup.selectAll('path.statePath').data(statesJson.features).enter().append('path').attr('d', path).attr('class', styles.statePath)
 		// Draw the circles
 		const pointsGroup = svg.append('g')
@@ -92,7 +93,10 @@ const SectorSelector: React.FC<ISectorSelectorProps> = ({ sectors, sector, onCha
 			.attr('class', styles.point)
 	}, [sectors, onChange, sector, d3config])
 
-	return <svg ref={svgRef} viewBox={`0 0 ${d3config.width} ${d3config.height}`} width="100%" height="100%" className={styles.SectorSelector}></svg>
+	if (!d3config) return <></>
+	const { width, height } = d3config
+
+	return <svg ref={svgRef} viewBox={`0 0 ${width} ${height}`} width="100%" height="100%" className={styles.SectorSelector}></svg>
 }
 
 export default SectorSelector
--- src/components/elements/SectorSelector/regions.tsx
+++ src/components/elements/SectorSelector/regions.tsx
@@ -1,30 +1,36 @@
 export const regions = {
 	CONUS: {
+		label: 'Continental US',
 		id: 'CONUS',
 		rotate: [98, -40],
 		scale: 2,
 	},
 	ALASKA: {
+		label: 'Alaska',
 		id: 'ALASKA',
 		rotate: [153, -63],
 		scale: 3,
 	},
 	HAWAII: {
+		label: 'Hawaii',
 		id: 'HAWAII',
 		rotate: [157, -21],
 		scale: 5,
 	},
 	PUERTO: {
+		label: 'Puerto Rico',
 		id: 'PUERTO',
 		rotate: [66, -18],
 		scale: 5,
 	},
 	GUAM: {
+		label: 'Guam',
 		id: 'GUAM',
 		rotate: [-145, -13.5],
 		scale: 25,
 	},
 	KOREA: {
+		label: 'Korea',
 		id: 'KOREA',
 		rotate: [-126, -30],
 		scale: 2,
