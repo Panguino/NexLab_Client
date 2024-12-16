7500e7e00bdfc25282f89efc9a8cd3a8adf7656a
Evan Anderson <112748748+elusiveuphoria@users.noreply.github.com>
2024-12-16 07:34:01 -0600

    Feature/nxl 7901073043 gif save (#53)
    * Initial Commit
    
    Working story, needs work
    
    * Better styling
    
    * Minor Improvments
    
    styling and partial refactor
    
    * Final adjustments
    
    * Button component updated and used
    
    * New Input Component
    
    broken out as independent component
    
    * removed deprecated styles
    
    * Added stories for new components
    
    * rename Input
    
    ---------
    
    Co-authored-by: Brad Larson <brad@bannerwave.com>

---
+++ src/components/blocks/SaveGIF/TestData.ts
@@ -0,0 +1,9 @@
+export const testFrames = [
+	'https://climate.cod.edu/storybook/nexrad/GRB.N0B.20241125.1900.gif',
+	'https://climate.cod.edu/storybook/nexrad/GRB.N0B.20241125.1907.gif',
+	'https://climate.cod.edu/storybook/nexrad/GRB.N0B.20241125.1914.gif',
+	'https://climate.cod.edu/storybook/nexrad/GRB.N0B.20241125.1921.gif',
+	'https://climate.cod.edu/storybook/nexrad/GRB.N0B.20241125.1928.gif',
+	'https://climate.cod.edu/storybook/nexrad/GRB.N0B.20241125.1935.gif',
+	'https://climate.cod.edu/storybook/nexrad/GRB.N0B.20241125.1942.gif',
+]
