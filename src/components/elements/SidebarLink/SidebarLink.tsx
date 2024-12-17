76983746b8daf5ef6aa05d936c4a310465113297
Evan Anderson <elusiveuphoria@gmail.com>
2024-12-17 03:44:14 -0600

    onClick and custom content loading added

--- src/components/elements/SidebarLink/SidebarLink.tsx
+++ src/components/elements/SidebarLink/SidebarLink.tsx
@@ -1,10 +1,44 @@
 import Link from 'next/link'
+import { MouseEventHandler, useCallback } from 'react'
 import styles from './SidebarLink.module.scss'
 
-export const SidebarLink = ({ name, linkUrl, target = '_self' }) => {
+interface ISidebarLinkProps {
+	name: string
+	linkUrl: string
+	target?: string
+	onClick?: (event: React.MouseEvent<HTMLAnchorElement>, contentElementId: string) => void
+}
+
+export const SidebarLink = ({ name, linkUrl, target = '_self', onClick }: ISidebarLinkProps) => {
+	const handleClick: MouseEventHandler<HTMLAnchorElement> = useCallback(
+		(event) => {
+			if (onClick) {
+				event.preventDefault()
+				const contentElementId = target
+				const contentElement = document.getElementById(contentElementId)
+				if (contentElement) {
+					fetch(linkUrl)
+						.then((response) => response.text())
+						.then((data) => {
+							contentElement.innerHTML = data
+						})
+						.catch((error) => {
+							console.error('Error loading content:', error)
+							alert('Failed to open link.')
+						})
+				} else {
+					console.error('Target element not found:', contentElementId)
+					alert('Failed to open link.')
+				}
+				onClick(event, contentElementId)
+			}
+		},
+		[linkUrl, target, onClick],
+	)
+
 	return (
 		<div className={styles.SidebarLink}>
-			<Link href={linkUrl} target={target}>
+			<Link href={linkUrl} target={target} onClick={onClick ? handleClick : undefined}>
 				{name}
 			</Link>
 		</div>
