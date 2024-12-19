c826f110c9530755e918c6919ecf39dba698e4a2
Brad Larson <brad@bannerwave.com>
2024-12-19 09:35:16 -0500

    Button styles

--- src/components/elements/Button/Button.stories.tsx
+++ src/components/elements/Button/Button.stories.tsx
@@ -1,6 +1,7 @@
-// Button.stories.tsx
+import buttonStyles from '@/styles/buttonStyles.module.scss'
 import { Meta, StoryFn } from '@storybook/react'
 import { Button } from './Button'
+import styles from './ButtonStory.module.scss'
 
 export default {
 	title: 'Components/Button',
@@ -11,7 +12,41 @@ export default {
 	},
 } as Meta
 
+const allButtonStyles = Object.keys(buttonStyles).map((key) => buttonStyles[key])
+
 const Template: StoryFn = (args) => <Button {...args} />
+const TemplateStyles: StoryFn = (args) => {
+	return (
+		<div className={styles.examples}>
+			<div className={styles.lightDark}>
+				<Button {...args} />
+				{allButtonStyles.map((style) => (
+					<Button {...args} label={style.split('_')[1]} variantClassName={style} />
+				))}
+			</div>
+			<div className={styles.blue}>
+				<Button {...args} />
+				{allButtonStyles.map((style) => (
+					<Button {...args} label={style.split('_')[1]} variantClassName={style} />
+				))}
+			</div>
+			<div className={styles.darkDark}>
+				<Button {...args} />
+				{allButtonStyles.map((style) => (
+					<Button {...args} label={style.split('_')[1]} variantClassName={style} />
+				))}
+			</div>
+		</div>
+	)
+}
+
+export const StyleExamples = TemplateStyles.bind({})
+StyleExamples.args = {
+	label: 'Example Button',
+	link: 'https://www.google.com',
+	target: '_blank',
+	disabled: false,
+}
 
 export const AsLink = Template.bind({})
 AsLink.args = {
@@ -36,13 +71,4 @@ Disabled.args = {
 	onClick: () => alert('This should not be clickable'),
 	style: '',
 	disabled: true,
-}
-
-export const OpenNewTab = Template.bind({})
-OpenNewTab.args = {
-	label: 'Open New Tab',
-	link: 'https://www.google.com',
-	target: '_blank',
-	style: '',
-	disabled: false,
 }
