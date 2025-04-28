c0299d0910b62c673eb26ebbc7b83d7af19b1316
Brad Larson <brad@bannerwave.com>
2025-04-28 16:03:14 -0400

    Animator Settings improvements

--- src/components/blocks/_animatorSettingPanels/NexradAnimatorSettings/NexradAnimatorSettings.tsx
+++ src/components/blocks/_animatorSettingPanels/NexradAnimatorSettings/NexradAnimatorSettings.tsx
@@ -9,12 +9,27 @@ const NexradAnimatorSettings = () => {
 	const setNexradFrameRate = useRootStore.use.setNexradFrameRate()
 	const setNexradNumberOfFrames = useRootStore.use.setNexradNumberOfFrames()
 
+	const frameDurationMs = 16 + (1000 - 16) * nexradFrameRate
+	const fps = 1000 / frameDurationMs
+
 	return (
 		<div className={styles.NexradAnimatorSettings}>
 			<p>Number Of Frames (1-200)</p>
-			<RangeInput minValue={1} maxValue={200} value={nexradNumberOfFrames} onChange={(value) => setNexradNumberOfFrames(value)} />
+			<div className={styles.group}>
+				<b>{nexradNumberOfFrames}</b>
+				<RangeInput minValue={1} maxValue={200} value={nexradNumberOfFrames} onChange={(value) => setNexradNumberOfFrames(value)} />
+			</div>
 			<p>Animation Speed (slow/fast)</p>
-			<RangeInput minValue={0.01} maxValue={1} value={nexradFrameRate} unitStep={0.01} onChange={(value) => setNexradFrameRate(value)} />
+			<div className={styles.group}>
+				<b>{fps >= 1 ? Math.floor(fps) : fps.toFixed(1)} fps</b>
+				<RangeInput
+					minValue={0.01}
+					maxValue={0.99}
+					value={1 - nexradFrameRate}
+					unitStep={0.01}
+					onChange={(value) => setNexradFrameRate(1 - value)}
+				/>
+			</div>
 		</div>
 	)
 }
