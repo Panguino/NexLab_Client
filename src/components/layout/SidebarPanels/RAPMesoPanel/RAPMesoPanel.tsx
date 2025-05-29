08bf0f2554a7862d57c96306f5150b5522fe964d
Evan Anderson <elusiveuphoria@gmail.com>
2025-05-29 05:15:45 -0500

    Rapmeso & Isentropic defaults defined

--- src/components/layout/SidebarPanels/RAPMesoPanel/RAPMesoPanel.tsx
+++ src/components/layout/SidebarPanels/RAPMesoPanel/RAPMesoPanel.tsx
@@ -4,14 +4,27 @@ import { SidebarGroup } from '@/components/elements/SidebarGroup/SidebarGroup'
 import { SidebarLink } from '@/components/elements/SidebarLink/SidebarLink'
 import { SidebarSectionHeader } from '@/components/elements/SidebarSectionHeader/SidebarSectionHeader'
 import SidebarPanelPad from '@/components/layout/SidebarPanelPad/SidebarPanelPad'
-import { ALL_RAPMESO_PRODUCTS } from '@/data/analysis/rap-mesoanalysis/products'
+import { ALL_RAPMESO_PRODUCTS, RAPMESO_PRODUCT_DEFAULT } from '@/data/analysis/rap-mesoanalysis/products'
+import { useParams, useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 import styles from './RAPMesoPanel.module.scss'
 
 interface RAPMesoPanelProps {
 	basepath: string
+	isActive?: boolean | false
 }
 
-export const RAPMesoPanel = ({ basepath }: RAPMesoPanelProps) => {
+export const RAPMesoPanel = ({ basepath, isActive }: RAPMesoPanelProps) => {
+	const router = useRouter()
+	const { rapmesoProductId: paramProductId } = useParams()
+	const productId = paramProductId ?? RAPMESO_PRODUCT_DEFAULT
+
+	useEffect(() => {
+		if (isActive && !ALL_RAPMESO_PRODUCTS[paramProductId as string]) {
+			router.push(`/weather-data/analysis/RAP-mesoanalysis/${RAPMESO_PRODUCT_DEFAULT}`)
+		}
+	}, [paramProductId, router, isActive])
+
 	const productsArray = Object.keys(ALL_RAPMESO_PRODUCTS).map((productId) => {
 		return { id: productId, label: ALL_RAPMESO_PRODUCTS[productId].label }
 	})
