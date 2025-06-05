7a44d83cf00b681ffef93726c347747b29e79f2e
Brad Larson <brad@bannerwave.com>
2025-06-05 13:36:20 -0400

    Responsive Updates (#103)
    * responsive improvements
    
    * responsive improvements

--- src/hooks/useDimensions.tsx
+++ src/hooks/useDimensions.tsx
@@ -1,43 +1,44 @@
-import { useLayoutEffect, useRef, useState } from 'react'
+import { useCallback, useLayoutEffect, useRef, useState } from 'react'
 
 const useDimensions = (aspectRatio?: number, contain: boolean = false) => {
 	const ref = useRef(null)
 	const [dimensions, setDimensions] = useState({ width: 0, height: 0, x: 0, y: 0, adjustedWidth: 0, adjustedHeight: 0 })
 
+	const updateDimensions = useCallback(() => {
+		if (!ref.current) return
+		const rect = ref.current.getBoundingClientRect()
+		const { width, height } = rect.toJSON()
+		let adjustedWidth = width
+		let adjustedHeight = height
+
+		if (aspectRatio) {
+			const containerAspectRatio = width / height
+			if (containerAspectRatio > aspectRatio) {
+				// Container is wider than the desired aspect ratio
+				if (contain) {
+					// Adjust width to fit inside the container
+					adjustedWidth = height * aspectRatio
+				} else {
+					// Bleed outside: Adjust height to fill the space
+					adjustedHeight = width / aspectRatio
+				}
+			} else if (containerAspectRatio < aspectRatio) {
+				// Container is taller than the desired aspect ratio
+				if (contain) {
+					// Adjust height to fit inside the container
+					adjustedHeight = width / aspectRatio
+				} else {
+					// Bleed outside: Adjust width to fill the space
+					adjustedWidth = height * aspectRatio
+				}
+			}
+			// If containerAspectRatio equals aspectRatio, no adjustment needed
+		}
+		setDimensions({ width, height, adjustedWidth, adjustedHeight, x: rect.x, y: rect.y })
+	}, [aspectRatio, contain])
+
 	useLayoutEffect(() => {
 		if (!ref.current) return () => {}
-
-		const updateDimensions = () => {
-			const rect = ref.current.getBoundingClientRect()
-			const { width, height } = rect.toJSON()
-			let adjustedWidth = width
-			let adjustedHeight = height
-
-			if (aspectRatio) {
-				const containerAspectRatio = width / height
-				if (containerAspectRatio > aspectRatio) {
-					// Container is wider than the desired aspect ratio
-					if (contain) {
-						// Adjust width to fit inside the container
-						adjustedWidth = height * aspectRatio
-					} else {
-						// Bleed outside: Adjust height to fill the space
-						adjustedHeight = width / aspectRatio
-					}
-				} else if (containerAspectRatio < aspectRatio) {
-					// Container is taller than the desired aspect ratio
-					if (contain) {
-						// Adjust height to fit inside the container
-						adjustedHeight = width / aspectRatio
-					} else {
-						// Bleed outside: Adjust width to fill the space
-						adjustedWidth = height * aspectRatio
-					}
-				}
-				// If containerAspectRatio equals aspectRatio, no adjustment needed
-			}
-			setDimensions({ width, height, adjustedWidth, adjustedHeight, x: rect.x, y: rect.y })
-		}
 
 		// Initialize ResizeObserver
 		const resizeObserver = new ResizeObserver(updateDimensions)
@@ -49,9 +50,9 @@ const useDimensions = (aspectRatio?: number, contain: boolean = false) => {
 		return () => {
 			resizeObserver.disconnect()
 		}
-	}, [aspectRatio, contain]) // Added contain to the dependency array
+	}, [aspectRatio, contain, updateDimensions]) // Added contain to the dependency array
 
-	return [ref, dimensions] as const
+	return [ref, dimensions, updateDimensions] as const
 }
 
 export default useDimensions
