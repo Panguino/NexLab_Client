e3ea515f1c76c0a07cda9ddd52032f718a75d6b4
Evan Anderson <elusiveuphoria@gmail.com>
2025-06-13 02:07:04 -0500

    ForecastAnimator plugged in and work

--- src/app/weather-data/forecast-models/[forecastModelId]/[forecastSectorId]/[forecastLevelId]/[forecastProductId]/page.tsx
+++ src/app/weather-data/forecast-models/[forecastModelId]/[forecastSectorId]/[forecastLevelId]/[forecastProductId]/page.tsx
@@ -1,5 +1,19 @@
-const Page = () => {
-	return <>Model content</>
-}
+import { getDataPageContent } from '@/apollo/strapi/getDataPageContent'
+import ForecastAnimator from '@/components/blocks/_animators/ForecastAnimator/ForecastAnimator'
+import { getForecastPageIdByParams } from '@/util/weatherDataPageLookup'
 
+const Page = async ({ params }) => {
+	const { forecastModelId: modelId, forecastLevelId: levelId, forecastProductId: productId } = params
+	const pageId = getForecastPageIdByParams(modelId, levelId, productId)
+	const pageData = await getDataPageContent(pageId)
+	return (
+		<ForecastAnimator
+			productInfo={{
+				info: pageData.productInfo || pageData.SEO.metaTitle,
+				image: pageData.ProductImage?.url || pageData.SEO.metaImage?.url,
+				description: pageData.productDescription || pageData.SEO.metaDescription,
+			}}
+		/>
+	)
+}
 export default Page
