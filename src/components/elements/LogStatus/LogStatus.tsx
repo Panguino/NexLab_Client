e2a6ffae6c094f756d9d020bed0723cab9683213
Brad Larson <brad@bannerwave.com>
2025-07-31 19:51:56 -0400

    avatar clicks through to dashboard

--- src/components/elements/LogStatus/LogStatus.tsx
+++ src/components/elements/LogStatus/LogStatus.tsx
@@ -1,12 +1,16 @@
 'use client'
 import { signOut } from 'next-auth/react'
+import Link from 'next/link'
 import Gravitar from 'react-gravatar'
 import styles from './LogStatus.module.scss'
 
 const LogStatus = ({ email }) => {
 	return (
 		<div className={styles.info}>
-			| <Gravitar email={email} size={40} className={styles.avatar} />{' '}
+			|{' '}
+			<Link href="/dashboard/">
+				<Gravitar email={email} size={40} className={styles.avatar} />
+			</Link>{' '}
 			<button className={styles.NavItemButton} onClick={() => signOut()}>
 				Log out
 			</button>
