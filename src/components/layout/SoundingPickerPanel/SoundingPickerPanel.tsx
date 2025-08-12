4a252610bcdc18f7cee48670aea81f02fdd240fb
Brad Larson <brad@bannerwave.com>
2025-08-12 13:45:30 -0400

    clicking sounding in location picker updates lat/long

--- src/components/layout/SoundingPickerPanel/SoundingPickerPanel.tsx
+++ src/components/layout/SoundingPickerPanel/SoundingPickerPanel.tsx
@@ -2,8 +2,10 @@
 
 import { Animator } from '@/components/elements/Animator/Animator'
 import { useRootStore } from '@/store/useRootStore'
+import { getLatLonFromXYandSector } from '@/util/forecast/common-functions'
 import { faClose } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useParams, useRouter } from 'next/navigation'
 import styles from './SoundingPickerPanel.module.scss'
 
 const SoundingPickerPanel = () => {
@@ -11,6 +13,25 @@ const SoundingPickerPanel = () => {
 	const closePanel = useRootStore.use.closeSoundingPicker()
 	const frames = useRootStore.use.soundingPickerFrames()
 	const imageInfo = useRootStore.use.soundingPickerImageInfo()
+	const router = useRouter()
+	const {
+		fcstModel: modelId,
+		fcstRun: runId,
+		fcstSector: sectorId,
+		fcstLevel: levelId,
+		fcstProduct: productId,
+		fcstSndValid: validTimeId,
+		fcstSndParcel: parcelId,
+		fcstSndWeather: weatherId,
+	} = useParams()
+
+	const onSoundingsClickthrough = ({ xPercent, yPercent }) => {
+		const locationId = getLatLonFromXYandSector(xPercent, yPercent, sectorId as string)
+		const baseParmsString = `${runId}/${modelId}/${sectorId}/${levelId}/${productId}`
+		const soundingParmsString = `${validTimeId}/${locationId}/${parcelId}/${weatherId}`
+		router.push(`/weather-data/forecast-models/${baseParmsString}/sounding/${soundingParmsString}`)
+		closePanel()
+	}
 
 	return (
 		<>
@@ -31,6 +52,9 @@ const SoundingPickerPanel = () => {
 										autoPlay={false}
 										startFrame={0}
 										disableZoom
+										soundingsPicker
+										soundingsPickerMode={true}
+										onSoundingsClickthrough={onSoundingsClickthrough}
 										zoomFill={false}
 									/>
 								</div>
