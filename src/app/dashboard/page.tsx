8c1477a76fe24de5a8c3c6be3ea9c7089e879947
Brad Larson <brad@bannerwave.com>
2025-09-29 08:27:28 -0400

    fix auth issues.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,10 @@
 import { DashboardSettings } from '@/components/blocks/DashboardSettings/DashboardSettings'
 import PageContentWrapper from '@/components/layout/PageContentWrapper/PageContentWrapper'
-import { getServerSession } from 'next-auth'
+import { auth } from '@/lib/auth'
 import { redirect } from 'next/navigation'
 
 const Page = async () => {
-	const session = await getServerSession()
+	const session = await auth()
 	if (!session || !session.user) {
 		redirect('/login')
 	}
