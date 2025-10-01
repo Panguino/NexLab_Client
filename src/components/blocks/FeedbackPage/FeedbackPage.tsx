de3b1538a3490c2868e59febafe35d8426b16073
Evan Anderson <elusiveuphoria@gmail.com>
2025-10-01 04:24:31 -0500

    added defined width to stepIcons - linting didnt like some imports

--- src/components/blocks/FeedbackPage/FeedbackPage.tsx
+++ src/components/blocks/FeedbackPage/FeedbackPage.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import ScrollArea from '@/components/layout/ScrollArea/ScrollArea'
 import { Footer } from '@/components/blocks/PageBlocks/Footer/Footer'
-import { FeedbackHero } from './FeedbackHero/FeedbackHero'
+import ScrollArea from '@/components/layout/ScrollArea/ScrollArea'
 import { DiscordRequirements } from './DiscordRequirements/DiscordRequirements'
+import { FeedbackHero } from './FeedbackHero/FeedbackHero'
 import styles from './FeedbackPage.module.scss'
 
 export const FeedbackPage = () => {
