6db47cc9c96e9ac39bfad215e1529a40f5fcb39c
Brad Larson <brad@bannerwave.com>
2025-10-20 20:02:47 -0400

    test: Set states color to pure red for debugging

--- src/components/elements/Animator/AnimatorMapMachine/AnimatorMapMachine.tsx
+++ src/components/elements/Animator/AnimatorMapMachine/AnimatorMapMachine.tsx
@@ -72,8 +72,8 @@ export const AnimatorMapMachine = forwardRef<HTMLDivElement, IAnimatorMapMachine
 		const oceanColor = isDark ? [35, 53, 68, 255] : [138, 173, 207, 255]
 		// World: grey2 (#d8d8d8) light / grey16 (#484848) dark
 		const worldColor = isDark ? [72, 72, 72, 255] : [216, 216, 216, 255]
-		// US States: white (#ffffff) light / grey13 (#5f5f5f) dark
-		const statesColor = isDark ? [95, 95, 95, 255] : [255, 255, 255, 255]
+		// US States: TEST - pure red
+		const statesColor = [255, 0, 0, 255]
 		// Borders: grey18 (#232323) light / grey15 (#505050) dark
 		const borderColor = isDark ? [80, 80, 80, 255] : [35, 35, 35, 255]
 
