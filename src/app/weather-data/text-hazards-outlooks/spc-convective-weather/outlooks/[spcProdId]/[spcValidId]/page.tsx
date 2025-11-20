065186748500eb5418cb4f44ae758ae5417238a0
Evan Anderson <112748748+elusiveuphoria@users.noreply.github.com>
2025-11-20 12:53:30 -0600

    Text - Convective Data Files, Page Structure, Mock Sidebar (#173)
    * product defining start
    
    * better plan for sidebar
    
    * testing path length
    
    * Sidebar Mockup
    
    * test use of data - placeholder sidebar - page structure established

---
+++ src/app/weather-data/text-hazards-outlooks/spc-convective-weather/outlooks/[spcProdId]/[spcValidId]/page.tsx
@@ -0,0 +1,14 @@
+import { ComingSoon } from '@/components/blocks/PageBlocks/ComingSoon/ComingSoon'
+
+const Page = () => {
+	return (
+		<ComingSoon
+			fullWidth
+			pageName="Convective Outlooks"
+			purpose="Displaying SPC convective outlook graphics and full text discussions, as well as an archive of past outlooks."
+			etaText="This section is being implemented."
+		/>
+	)
+}
+
+export default Page
---
+++ src/app/weather-data/text-hazards-outlooks/spc-convective-weather/reports/page.tsx
@@ -0,0 +1,14 @@
+import { ComingSoon } from '@/components/blocks/PageBlocks/ComingSoon/ComingSoon'
+
+const Page = () => {
+	return (
+		<ComingSoon
+			fullWidth
+			pageName="Local Storm Reports"
+			purpose="Table of combined NWS local storm reports. Rebuild of existing Bootstrap Table."
+			etaText="This section is being implemented."
+		/>
+	)
+}
+
+export default Page
