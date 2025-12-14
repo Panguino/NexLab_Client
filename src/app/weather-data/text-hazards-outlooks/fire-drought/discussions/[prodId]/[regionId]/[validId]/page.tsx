96c1fee95cc85769b4d191717268faa567257a12
Evan Anderson <elusiveuphoria@gmail.com>
2025-12-14 01:19:54 -0500

    page structure and data started

---
+++ src/app/weather-data/text-hazards-outlooks/fire-drought/discussions/[prodId]/[regionId]/[validId]/page.tsx
@@ -0,0 +1,14 @@
+import { ComingSoon } from '@/components/blocks/PageBlocks/ComingSoon/ComingSoon'
+
+const Page = () => {
+	return (
+		<ComingSoon
+			fullWidth
+			pageName="FWO and USDM Discussions"
+			purpose="The Fire Weather Outlook (FWO) and U.S. Drought Monitor (USDM) summaries, including graphics and discussions."
+			etaText="This section is being implemented."
+		/>
+	)
+}
+
+export default Page
---
+++ src/app/weather-data/text-hazards-outlooks/fire-drought/hazards/page.tsx
@@ -0,0 +1,14 @@
+import { ComingSoon } from '@/components/blocks/PageBlocks/ComingSoon/ComingSoon'
+
+const Page = () => {
+	return (
+		<ComingSoon
+			fullWidth
+			pageName="Fire and Drought Products"
+			purpose="Discussions, outlooks, hazards and graphical analyis products related to fire and drought."
+			etaText="This section is being implemented."
+		/>
+	)
+}
+
+export default Page
