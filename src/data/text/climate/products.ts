c2ec0395382516f2073e546515f30798fe0a9636
Evan Anderson <elusiveuphoria@gmail.com>
2026-01-12 15:08:25 -0600

    product iteration

---
+++ src/data/text/climate/products.ts
@@ -0,0 +1,30 @@
+// <option
+//     data-thumb="../assets/images/icons/onlytext.png"
+//     data-gfx="none"
+//     data-txturl="{{ url_for('rt_main.product', office='KWBC', product='FXUS06_PMDMRD', _external=True) }}"
+// >6-14 Day Discussion</option>
+// <option
+//     data-thumb="../assets/images/icons/onlytext.png"
+//     data-gfx="none"
+//     data-txturl="{{ url_for('rt_main.product', office='KWBC', product='FXUS07_PMD30D', _external=True) }}"
+// >Monthly Outlook</option>
+// <option
+//     data-thumb="../assets/images/icons/onlytext.png"
+//     data-gfx="none"
+//     data-txturl="{{ url_for('rt_main.product', office='KWBC', product='FXUS05_PMD90D', _external=True) }}"
+// >Long-Lead Seasonal Outlooks</option>
+// <option
+//     data-thumb="../assets/images/icons/onlytext.png"
+//     data-gfx="none"
+//     data-txturl="{{ url_for('rt_main.product', office='KWBC', product='FXHW40_PMDHCO', _external=True) }}"
+// >Long-Lead Hawaiian Outlooks</option>
+// <option
+//     data-thumb="../assets/images/icons/onlytext.png"
+//     data-gfx="none"
+//     data-txturl="{{ url_for('rt_main.product', office='KWNC', product='FXUS23_PMDSST', _external=True) }}"
+// >Tropical Pacific Mean SST Outlook</option>
+export const CLIMATE_TEXT_PRODUCT_6_14_OUTLOOK_ID = '6_14_OUTLOOK'
+export const CLIMATE_TEXT_PRODUCT_MONTHLY_OUTLOOK_ID = 'MONTHLY_OUTLOOK'
+export const CLIMATE_TEXT_PRODUCT_SEASONAL_OUTLOOK_ID = 'SEASONAL_OUTLOOK'
+export const CLIMATE_TEXT_PRODUCT_HAWAIIAN_OUTLOOK_ID = 'HAWAIIAN_OUTLOOK'
+export const CLIMATE_TEXT_PRODUCT_TROPICAL_PACIFIC_SST_ID = 'TROPICAL_PACIFIC_SST'
