95b33e7134ee2d4a98cc1a653ec9b342f5b462ed
Evan Anderson <112748748+elusiveuphoria@users.noreply.github.com>
2026-02-27 23:48:51 -0600

    possible all clear corrective measures (#192)

--- src/data/text/climate/sectors.ts
+++ src/data/text/climate/sectors.ts
@@ -1,12 +1,12 @@
 export const CLIMATE_TEXT_SSTOLR_SECTOR_WORLD_ID = 'world'
-export const CLIMATE_TEXT_SSTOLR_SECTOR_ATL_GLB_ID = 'atl-glb'
+export const CLIMATE_TEXT_SSTOLR_SECTOR_ATL_GLB_ID = 'atlglb'
 export const CLIMATE_TEXT_SSTOLR_SECTOR_GULF_ID = 'gulf'
-export const CLIMATE_TEXT_SSTOLR_SECTOR_IND_GLB_ID = 'ind-glb'
+export const CLIMATE_TEXT_SSTOLR_SECTOR_IND_GLB_ID = 'indglb'
 export const CLIMATE_TEXT_SSTOLR_SECTOR_NATL_ID = 'natl'
-export const CLIMATE_TEXT_SSTOLR_SECTOR_NEPAC_GLB_ID = 'nepac-glb'
+export const CLIMATE_TEXT_SSTOLR_SECTOR_NEPAC_GLB_ID = 'nepacglb'
 export const CLIMATE_TEXT_SSTOLR_SECTOR_NPAC_ID = 'npac'
 export const CLIMATE_TEXT_SSTOLR_SECTOR_NPOLE_ID = 'npole'
-export const CLIMATE_TEXT_SSTOLR_SECTOR_NWPAC_GLB_ID = 'nwpac-glb'
+export const CLIMATE_TEXT_SSTOLR_SECTOR_NWPAC_GLB_ID = 'nwpacglb'
 export const CLIMATE_TEXT_SSTOLR_SECTOR_SPOLE_ID = 'spole'
 
 export const CLIMATE_TEXT_SSTOLR_SECTORS = [
